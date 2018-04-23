3d7e5e7ede1c1952e8eed8d5a609e6d8f8aca7d3
Jean Jourdain <jean.jourdain@mixio.com>
2018-04-23 11:37:02 +0200

    configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,47 +1,39 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
-/// Called before your application initializes.
-///
-/// https://docs.vapor.codes/3.0/getting-started/structure/#configureswift
-public func configure(
-    _ config: inout Config,
-    _ env: inout Environment,
-    _ services: inout Services
-) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
-
-    /// Register routes to the router
+public func configure (_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    
+    try services.register(FluentPostgreSQLProvider())
+    
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
-    /// Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    
+    var middlewares = MiddlewareConfig()
+    middlewares.use(DateMiddleware.self)
+    middlewares.use(ErrorMiddleware.self)
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite: SQLiteDatabase
-    if env.isRelease {
-        /// Create file-based SQLite db using $SQLITE_PATH from process env
-        sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!))
-    } else {
-        /// Create an in-memory SQLite database
-        sqlite = try SQLiteDatabase(storage: .memory)
-    }
-
-    /// Register the configured SQLite database to the database config.
+    
+    // Configure a database.
     var databases = DatabaseConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let database = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        username: username,
+        database: database,
+        password: password
+    )
+    let postgreSQLDatabase = PostgreSQLDatabase(config: databaseConfig)
+    databases.add(database: postgreSQLDatabase, as: .psql)
     services.register(databases)
-
-    /// Configure migrations
+    
     var migrations = MigrationConfig()
-    migrations.add(model: Acronym.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
-
+    
 }
