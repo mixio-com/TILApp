f601782f6a0e01f444610f7c376f3e3080c07f28
Jean Jourdain <jean.jourdain@mixio.com>
2018-04-23 11:40:07 +0200

    PostgreSQL version

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,29 +1,19 @@
 import Vapor
-import FluentSQLite
+import FluentPostgreSQL
 
 final class Acronym: Codable {
-
+    
     var id: Int?
     var short: String
     var long: String
-
-    init(short:String, long: String) {
+    
+    init(short: String, long: String) {
         self.short = short
         self.long = long
     }
-
+    
 }
 
-//extension Acronym: Model {
-//
-//    typealias Database = SQLiteDatabase
-//    typealias ID = Int
-//
-//    public static var idKey: IDKey = \Acronym.id
-//}
-
-extension Acronym: SQLiteModel {}
-
+extension Acronym: PostgreSQLModel {}
 extension Acronym: Migration {}
-
 extension Acronym: Content {}
