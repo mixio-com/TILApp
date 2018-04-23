d4b54275f1d247b207d1422a08b614e39dde1795
Jean Jourdain <jean.jourdain@mixio.com>
2018-04-23 11:34:49 +0200

    Added a get /api/acronyms route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,12 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
 
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+
+            return Acronym.query(on: req).all()
+
+    }
+
     router.post("api", "acronyms") { req -> Future<Acronym> in
 
         return try req.content.decode(Acronym.self).flatMap(to: Acronym.self) { acronym in
