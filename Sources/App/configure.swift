d1f2ce006be99f6a1a4f0ff182a9ba73f6213c31
Jean Jourdain <jean.jourdain@mixio.com>
2018-05-18 08:49:38 +0200

    Added tests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,18 +21,31 @@ public func configure (_ config: inout Config, _ env: inout Environment, _ servi
     
     // Configure a database.
     var databases = DatabasesConfig()
-    
+
+    let databaseName: String
+    let databasePort: Int
+    // 1
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let database = Environment.get("DATABASE_DB") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
 
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
+        // 2
+        port : databasePort,
         username: username,
-        database: database,
+        database: databaseName,
         password: password
     )
+
     let postgreSQLDatabase = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: postgreSQLDatabase, as: .psql)
     services.register(databases)
