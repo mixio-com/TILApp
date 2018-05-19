2b87baa35b42b5885702f1a8cf30b45596b56268
Jean Jourdain <jean.jourdain@mixio.com>
2018-05-19 19:58:51 +0200

    Testing

--- Tests/AppTests/Application+Testable.swift
+++ Tests/AppTests/Application+Testable.swift
@@ -1,9 +1,36 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
 import Vapor
 import App
 import FluentPostgreSQL
 
-extension Application{
-    
+extension Application {
     static func testable(envArgs: [String]? = nil) throws -> Application {
         var config = Config.default()
         var services = Services.default()
@@ -15,47 +42,35 @@ extension Application{
 
         try App.configure(&config, &env, &services)
         let app = try Application(config: config, environment: env, services: services)
+
         try App.boot(app)
         return app
     }
 
     static func reset() throws {
         let revertEnvironment = ["vapor", "revert", "--all", "-y"]
-        try Application.testable(envArgs: revertEnvironment)
-            .asyncRun()
-            .wait()
+        try Application.testable(envArgs: revertEnvironment).asyncRun().wait()
     }
-    
-    //1
-    func sendRequest(to path: String,
-                     method: HTTPMethod,
-                     headers: HTTPHeaders = .init(),
-                     body: HTTPBody = .init()) throws -> Response
-    {
-        // 2
+
+    func sendRequest(to path: String, method: HTTPMethod, headers: HTTPHeaders = .init(), body: HTTPBody = .init()) throws -> Response {
         let responder = try self.make(Responder.self)
         let request = HTTPRequest(method: method, url: URL(string: path)!, headers: headers, body: body)
         let wrappedRequest = Request(http: request, using: self)
-        // 3
         return try responder.respond(to: wrappedRequest).wait()
     }
 
-    // 4
-    func getResponse<T>(
-        to path: String,
-        method: HTTPMethod = .GET,
-        headers: HTTPHeaders = .init(),
-        body: HTTPBody = .init(),
-        decodeTo type: T.Type
-        ) throws -> T where T: Decodable {
-        let response = try self.sendRequest(
-            to: path,
-            method: method,
-            headers: headers,
-            body: body
-        )
-        // 6
+    func getResponse<T>(to path: String, method: HTTPMethod = .GET, headers: HTTPHeaders = .init(), body: HTTPBody = .init(), decodeTo type: T.Type) throws -> T where T: Decodable {
+        let response = try self.sendRequest(to: path, method: method, headers: headers, body: body)
         return try JSONDecoder().decode(type, from: response.http.body.data!)
     }
 
+    func getResponse<T, U>(to path: String, method: HTTPMethod = .GET, headers: HTTPHeaders = .init(), data: U, decodeTo type: T.Type) throws -> T where T: Decodable, U: Encodable {
+        let body = try HTTPBody(data: JSONEncoder().encode(data))
+        return try self.getResponse(to: path, method: method, headers: headers, body: body, decodeTo: type)
+    }
+
+    func sendRequest<T>(to path: String, method: HTTPMethod, headers: HTTPHeaders, data: T) throws where T: Encodable {
+        let body = try HTTPBody(data: JSONEncoder().encode(data))
+        _ = try self.sendRequest(to: path, method: method, headers: headers, body: body)
+    }
 }
--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -96,4 +96,84 @@ final class UserTests: XCTestCase {
 //        conn.close()
 //    }
 
+    func testUserCanBeSavedWithAPI() throws {
+        // 1
+        let user = User(name: usersName, username: usersUsername)
+
+        // 2
+        let receivedUser = try app.getResponse(
+            to: usersURI,
+            method: .POST,
+            headers: ["Content-Type": "application/json"],
+            data: user,
+            decodeTo: User.self
+        )
+        
+        // 3
+        XCTAssertEqual(receivedUser.name, usersName)
+        XCTAssertEqual(receivedUser.username, usersUsername)
+        XCTAssertNotNil(receivedUser.id)
+
+        // 4
+        let users = try app.getResponse(to: usersURI, decodeTo: [User].self)
+
+        // 5
+        XCTAssertEqual(users.count, 1)
+        XCTAssertEqual(users[0].name, usersName)
+        XCTAssertEqual(users[0].username, usersUsername)
+        XCTAssertEqual(users[0].id, receivedUser.id)
+    }
+
+    func testGettingASingleUserFromTheAPI() throws {
+        // 1
+        let user = try User.create(name: usersName, username: usersUsername, on: conn)
+
+        // 2
+        let receivedUser = try app.getResponse(to: "\(usersURI)\(user.id!)", decodeTo: User.self)
+
+        // 3
+        XCTAssertEqual(receivedUser.name, usersName)
+        XCTAssertEqual(receivedUser.username, usersUsername)
+        XCTAssertEqual(receivedUser.id, user.id)
+    }
+
+    func testGettingAUsersAcronymsFromTheAPI() throws {
+        // 1
+        let user = try User.create(on: conn)
+
+        // 2
+        let acronymShort = "OMG"
+        let acronymLong = "Oh My God!"
+
+        // 3
+        let acronym1 = try Acronym.create(
+            short: acronymShort,
+            long: acronymLong,
+            user: user,
+            on:conn
+        )
+        _ = try Acronym.create(
+            short: "LOL",
+            long: "Laughing Out Loud",
+            user: user,
+            on: conn
+        )
+
+        // 4
+        let acronyms = try app.getResponse(to: "\(usersURI)\(user.id!)/acronyms", decodeTo: [Acronym].self)
+
+        // 5
+        XCTAssertEqual(acronyms.count, 2)
+        XCTAssertEqual(acronyms[0].id, acronym1.id)
+        XCTAssertEqual(acronyms[0].short, acronymShort)
+        XCTAssertEqual(acronyms[0].long, acronymLong)
+    }
+
+    static let allTests = [
+        ("testUsersCanBeRetrievedFromAPI", testUsersCanBeRetrievedFromAPI),
+        ("testUserCanBeSavedWithAPI", testUserCanBeSavedWithAPI),
+        ("testGettingASingleUserFromTheAPI", testGettingASingleUserFromTheAPI),
+        ("testGettingAUsersAcronymsFromTheAPI", testGettingAUsersAcronymsFromTheAPI),
+    ]
+
 }
