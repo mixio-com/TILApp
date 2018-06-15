d57121d717ff2ee126bfe26eef3d303e6a21b6e8
Jean Jourdain <jean.jourdain@mixio.com>
2018-06-15 09:32:53 +0200

    Convert to User.Public

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -4,7 +4,6 @@ import Crypto
 struct UsersController: RouteCollection {
 
     func boot(router: Router) throws {
-
         let usersRoutes = router.grouped("api", "users")
         usersRoutes.post(User.self, use: createHandler)
         usersRoutes.get(use: getAllHandler)
@@ -12,59 +11,41 @@ struct UsersController: RouteCollection {
         usersRoutes.get(User.parameter, "acronyms", use: getAcronymsHandler)
         usersRoutes.delete(User.parameter, use: deleteHandler)
         usersRoutes.put(User.parameter, use: updateHandler)
-
     }
 
-    func createHandler(_ req: Request, user: User) throws -> Future<User> {
-
+    func createHandler(_ req: Request, user: User) throws -> Future<User.Public> {
         user.password = try BCrypt.hash(user.password)
-        return user.save(on: req)
-
+        return user.save(on: req).convertToPublic()
     }
 
-    func getAllHandler(_ req: Request) throws -> Future<[User]> {
-
-        return User.query(on: req).all()
-
+    func getAllHandler(_ req: Request) throws -> Future<[User.Public]> {
+        return User.query(on: req).all().map(to: [User.Public].self) { users in
+            return users.map { $0.convertToPublic() }
+        }
     }
 
-    func getHandler(_ req: Request) throws -> Future<User> {
-
-        return try req.parameters.next(User.self)
-
+    func getHandler(_ req: Request) throws -> Future<User.Public> {
+        return try req.parameters.next(User.self).convertToPublic()
     }
 
     func getAcronymsHandler(_ req: Request) throws -> Future<[Acronym]> {
-
         return try req.parameters.next(User.self).flatMap(to: [Acronym].self) { user in
-
             try user.acronyms.query(on: req).all()
-
         }
-
     }
 
     func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
-
         return try req.parameters.next(User.self).flatMap(to: HTTPStatus.self) { user in
-
             return user.delete(on:req).transform(to: HTTPStatus.noContent)
         }
-
     }
 
-    func updateHandler(_ req: Request) throws -> Future<User> {
-
-        return try flatMap(to: User.self, req.parameters.next(User.self), req.content.decode(User.self)) { user, updatedUser in
-
+    func updateHandler(_ req: Request) throws -> Future<User.Public> {
+        return try flatMap(to: User.Public.self, req.parameters.next(User.self), req.content.decode(User.self)) { user, updatedUser in
             user.name = updatedUser.name
             user.username = updatedUser.username
-
-            return user.save(on: req)
-
+            return user.save(on: req).convertToPublic()
         }
-
     }
-
 
 }
